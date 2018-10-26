27a37a2c8fc648835c3d82443fb9fbc685da330e
xGioe <giovannimaria.riva@studenti.unitn.it>
2018-10-26 17:36:12 +0200

    pull modifies

--- solidity-experiments/contracts/test.sol
+++ solidity-experiments/contracts/test.sol
@@ -7,37 +7,57 @@ contract Purchase {
         uint id;
         string color;
     }
-
+    
     //mapping diamonds
     mapping (uint => Diamond[]) diamonds;
     //uint[] public diamondList;
 
     address merchant;
+    address buyer;
+    address seller; 
+    address agent; 
 
 
-    function Purchase() {
+    function Purchase(address _agent, address _seller) { 
         merchant = msg.sender;
+        agent = _agent;
+        buyer = _buyer; 
+        seller = _seller; 
     }
 
-    function addDiamond(uint id, address owner, uint id2, string color) public {
+    function release() { 
+        if (msg.sender == agent) 
+            suicide(seller); // Send all funds to seller
+            //call change ownership 
+        else throw; 
+    }
+
+    function cancel() { 
+        if (msg.sender == agent) 
+            suicide(buyer); // Cancel escrow and return all funds to buyer
+            //no need to call change ownership    
+        else throw; 
+    }  
+
+    function addDiamond(uint id, address owner, uint id, string color) public {
         //var diamond = diamonds[_uint];
 
         //diamond.address = _age;
         //diamond.id = id;
         //diamond.color = color;
-    if (msg.sender == merchant)
-        diamonds[id].push(Diamond(owner, id2, color));
+    if (msg.sender == merchant)   
+        diamonds[id].push(Diamond(owner, id, color));
     else throw;
-
     }
 
-    // function getDiamonds() view public returns (uint[]) {
-    //     return diamonds;
-    // }
-    //
-    // function countDiamonds() view public returns (uint) {
-    //     return diamonds.length;
-    // }
+
+    function getDiamonds() view public returns (uint[]) {
+        return diamonds;
+    }
+
+    function countDiamonds() view public returns (uint) {
+        return diamondList.length;
+    }
 
     /*
     function changeOwner(Diamond diamond, address owner) {
@@ -45,11 +65,11 @@ contract Purchase {
             diamond.owner = owner;
         else throw;
     }
+    
     */
-
     function changeOwner(uint id, uint index, address owner) public {
         if (msg.sender == merchant)
             diamonds[id][index].owner = owner;
-        else throw;
+        else throw; 
     }
 }
