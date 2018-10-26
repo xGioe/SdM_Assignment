9dfdba2b89d88166991551b7b0f9be5dcba91226
xGioe <giovannimaria.riva@studenti.unitn.it>
2018-10-26 17:40:14 +0200

    update truffle js

--- solidity-experiments/truffle.js
+++ solidity-experiments/truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -15,4 +15,17 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  rpc: {
+      host:"localhost",
+      port:8545
+  },
+  networks: {
+      development: {
+          host: "localhost", //our network is running on localhost
+          port: 8545, // port where your blockchain is running
+          network_id: "*",
+          from: "0x8cc5a1a0802db41db826c2fcb72423744338dcb0", // use the account-id generated during the setup process
+         // gas: 20000000
+      }
+  }
 };
