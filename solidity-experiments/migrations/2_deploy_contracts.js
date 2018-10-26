942fd53a3de048933d6c53da6a85d15761c9853d
xGioe <giovannimaria.riva@studenti.unitn.it>
2018-10-26 17:04:12 +0200

    bugfix

--- solidity-experiments/migrations/2_deploy_contracts.js
+++ solidity-experiments/migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 var purchase = artifacts.require("../contracts/purchase.sol");
 var escrow = artifacts.require("../contracts/escrow.sol");
+var test = artifacts.require("../contracts/test.sol");
 module.exports = function(deployer) {
    deployer.deploy(purchase);
    deployer.deploy(escrow, '0x3590aca93338b0721966a8d0c96ebf2c4c87c544', '0x4e79423a920def8a35f312c9632cd6429025e44c');
+   deployer.deploy(test);
 };
