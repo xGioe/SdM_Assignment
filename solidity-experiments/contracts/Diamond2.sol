2976d4f177e9f6f10899c1cd0302d707213938b4
xGioe <giovannimaria.riva@studenti.unitn.it>
2018-11-03 17:08:46 +0100

    add selling function. Test it on remix

--- solidity-experiments/contracts/Diamond2.sol
+++ solidity-experiments/contracts/Diamond2.sol
@@ -1,120 +1,153 @@
 pragma solidity ^0.4.22;
 //pragma experimental ABIEncoderV2;
 
-contract DiamondTracker {
+contract DiamondTracker2 {
 
-    struct Diamond {
-        bytes32 id;
-        string origin;
-        DiamondType d_type;
-        DiamondProperties properties;
+  struct Diamond {
+    bytes32 id;
+    string origin;
+    DiamondType d_type;
+    DiamondProperties properties;
+  }
+
+  struct DiamondProperties {
+    uint size;
+    //TODO rest of the properties
+  }
+
+  enum DiamondType { Synthetic, Natural }
+
+  address[] certificate_authorities;
+  // mapping(bytes32 => address) owners;
+  //the mapping between each owner (address) and the diamons possessed
+  mapping (address => Diamond[]) public owners;
+
+  Diamond[] diamondsList;
+
+
+  event diamondSold();
+
+  constructor(address[] _certificate_authorities) public {
+    certificate_authorities = _certificate_authorities;
+  }
+
+  //TODO increase number of properties
+  //Type 1 = Synthetic, Type 2 = Natural
+  function register(address _owner, uint _type, string _origin, uint _size) external returns (bytes32) {
+    require(isCA(msg.sender), "You are not allowed to call register()");
+    require(_type == 1 || _type == 2, "Type must be 1(Synthetic) or 2(Natural)");
+
+    Diamond memory d;
+    if(_type == 1) {
+      d.d_type = DiamondType.Synthetic;
+    } else if (_type == 2) {
+      d.d_type = DiamondType.Natural;
     }
+    d.origin = _origin;
+    d.properties.size = _size;
+    //creation of the unique ID
+    d.id = sha256(abi.encodePacked(_size, _type));
 
-    struct DiamondProperties {
-        uint size;
-        //TODO rest of the properties
-    }
+    if(!addDiamond(d, _owner))
+    revert("Diamond already exists");
 
-    enum DiamondType { Synthetic, Natural }
+    return d.id;
+  }
 
-    address[] certificate_authorities;
-    // mapping(bytes32 => address) owners;
-    //the mapping between each owner (address) and the diamons possessed
-    mapping (address => Diamond[]) public owners;
+  function sellDiamond(bytes32 ID, address newOwner) {
+    Diamond sellingDiamond;
+    sellingDiamond.id = ID;
+    require(isOwner(msg.sender, sellingDiamond), "You are not the owner of the specified diamond");
 
-    Diamond[] diamondsList;
-
-    constructor(address[] _certificate_authorities) public {
-        certificate_authorities = _certificate_authorities;
-    }
-
-    //TODO increase number of properties
-    //Type 1 = Synthetic, Type 2 = Natural
-    function register(address _owner, uint _type, string _origin, uint _size) external returns (bytes32) {
-        require(isAllowed(msg.sender), "You are not allowed to call register()");
-        require(_type == 1 || _type == 2, "Type must be 1(Synthetic) or 2(Natural)");
-
-        Diamond memory d;
-        if(_type == 1) {
-            d.d_type = DiamondType.Synthetic;
-        } else if (_type == 2) {
-            d.d_type = DiamondType.Natural;
-        }
-        d.origin = _origin;
-        d.properties.size = _size;
-        d.id = sha256(abi.encodePacked(_size, _type));
-
-        if(!addDiamond(d, _owner))
-            revert("Diamond already exists");
-
-        return d.id;
-    }
-
-    // Returns the list of registered diamonds
-    // function getDiamondsList() external view returns (Diamond[]){
-    //   return diamondsList;
-    // }
-
-    function getDiamondByIndex(uint index) external view returns (bytes32, string, DiamondType, uint) {
-        return (
-            diamondsList[index].id,
-            diamondsList[index].origin,
-            diamondsList[index].d_type,
-            diamondsList[index].properties.size
-        );
-    }
-
-    function getDiamondById(bytes32 id) external view returns (bytes32, string, DiamondType, uint) {
-      for(uint i = 0; i < diamondsList.length; i++) {
-          if(diamondsList[i].id == id)
-          return (
-              diamondsList[i].id,
-              diamondsList[i].origin,
-              diamondsList[i].d_type,
-              diamondsList[i].properties.size
-          );
+    Diamond[] ownedDiamonds = owners[msg.sender];
+    for(uint i = 0; i < ownedDiamonds.length; i++) {
+      if(owners[msg.sender][i].id == sellingDiamond.id){
+        delete owners[msg.sender][i];
       }
     }
 
-    function getNumberOfDiamonds() external view returns (uint) {
-        return diamondsList.length;
-    }
-
-    function equals(Diamond d1, Diamond d2) internal pure returns (bool) {
-        return keccak256(encodeDiamond(d1)) == keccak256(encodeDiamond(d2));
-    }
-
-    function addDiamond(Diamond d, address owner) private returns (bool) {
-        //TODO I think this can be optimised
-        for(uint i = 0; i < diamondsList.length; i++) {
-            if(equals(d, diamondsList[i]))
-                return false;
+    for(uint j = 0; j < diamondsList.length; j++) {
+        if (diamondsList[j].id == sellingDiamond.id){
+                owners[newOwner].push(diamondsList[j]);
         }
-
-        // NOTE: we expect that if diamond is already in diamondsList then it has a owner
-        // might be a security issue of non-owenership
-        diamondsList.push(d);
-        // owners[d.id] = owner;
-
-        owners[owner].push(d);
-        return true;
     }
 
-    function isAllowed(address user) private view returns (bool) {
-        for(uint i = 0; i < certificate_authorities.length; i++) {
-            if(certificate_authorities[i] == user)
-                return true;
-        }
+    emit diamondSold();
 
-        return false;
-    }
+  }
 
-    function encodeDiamond(Diamond d) private pure returns (bytes) {
-        return abi.encodePacked(
-            d.id,
-            d.origin,
-            d.d_type,
-            d.properties.size
+
+
+  function getDiamondByIndex(uint index) external view returns (bytes32, string, DiamondType, uint) {
+    return (
+      diamondsList[index].id,
+      diamondsList[index].origin,
+      diamondsList[index].d_type,
+      diamondsList[index].properties.size
+    );
+  }
+
+  function getDiamondByDiamonId(bytes32 id) external view returns (bytes32, string, DiamondType, uint) {
+    for(uint i = 0; i < diamondsList.length; i++) {
+      if(diamondsList[i].id == id){
+        return (
+          diamondsList[i].id,
+          diamondsList[i].origin,
+          diamondsList[i].d_type,
+          diamondsList[i].properties.size
         );
+      }
     }
+  }
+
+  function getNumberOfDiamonds() external view returns (uint) {
+    return diamondsList.length;
+  }
+
+  function equals(Diamond d1, Diamond d2) internal pure returns (bool) {
+    return keccak256(encodeDiamond(d1)) == keccak256(encodeDiamond(d2));
+  }
+
+  function addDiamond(Diamond d, address owner) private returns (bool) {
+    //TODO I think this can be optimised
+    for(uint i = 0; i < diamondsList.length; i++) {
+      if(equals(d, diamondsList[i]))
+      return false;
+    }
+
+    // NOTE: we expect that if diamond is already in diamondsList then it has a owner
+    diamondsList.push(d);
+    // owners[d.id] = owner;
+
+    owners[owner].push(d);
+    return true;
+  }
+
+  function isCA(address user) private view returns (bool) {
+    for(uint i = 0; i < certificate_authorities.length; i++) {
+      if(certificate_authorities[i] == user)
+      return true;
+    }
+
+    return false;
+  }
+
+  function isOwner(address user, Diamond sellingDiamond) private view returns (bool) {
+    Diamond[] ownedDiamonds = owners[user];
+    for(uint i = 0; i < ownedDiamonds.length; i++) {
+      if(ownedDiamonds[i].id == sellingDiamond.id){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function encodeDiamond(Diamond d) private pure returns (bytes) {
+    return abi.encodePacked(
+      d.id,
+      d.origin,
+      d.d_type,
+      d.properties.size
+    );
+  }
 }
--- solidity-experiments/migrations/2_deploy_contracts.js
+++ solidity-experiments/migrations/2_deploy_contracts.js
@@ -1,4 +1,7 @@
-var Purchase = artifacts.require("./Purchase.sol");
-module.exports = function(deployer) {   
-   deployer.deploy(Purchase);
+var DiamondTracker2 = artifacts.require("./DiamondTracker2.sol");
+
+var certificate_authorities = ["0x8cc5a1a0802db41db826c2fcb72423744338dcb0","0x3590aca93338b0721966a8d0c96ebf2c4c87c544"];
+
+module.exports = function(deployer) {
+   deployer.deploy(DiamondTracker2, certificate_authorities);
 };
