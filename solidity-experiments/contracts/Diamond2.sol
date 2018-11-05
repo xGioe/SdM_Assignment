2dd81db329476f9c5a4be172e67f9a0ac9351071
fedefe <federicocasano@gmail.com>
2018-11-05 14:09:14 +0100

    Add files via upload

--- solidity-experiments/contracts/Diamond2.sol
+++ solidity-experiments/contracts/Diamond2.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.4.22;
-//pragma experimental ABIEncoderV2;
 
 contract DiamondTracker2 {
 
   struct Diamond {
-    bytes32 id;  //use bytes for arbitrary-length raw byte data and string for arbitrary-length string (UTF-8) data
+    bytes32 id;  
     string origin;
     DiamondType d_type;
     DiamondProperties properties;
@@ -12,20 +11,14 @@ contract DiamondTracker2 {
 
   struct DiamondProperties {
     uint size;
-    //string color;
-    //string clarity;
-    //string cut;
-    //string carat; //fixed points numbers are not supported yet by solidity
-    //TODO rest of the properties
   }
 
   enum DiamondType { Synthetic, Natural }
   address[] certificate_authorities;
   // mapping(bytes32 => address) owners;
   //the mapping between each owner (address) and the diamons possessed
-  mapping (address => Diamond[]) public owners;
+  mapping (address => Diamond[]) public owners;  
   Diamond[] diamondsList;
-
 
   event diamondSold();
 
@@ -33,12 +26,9 @@ contract DiamondTracker2 {
     certificate_authorities = _certificate_authorities;
   }
 
-  //TODO increase number of properties
-  //Type 1 = Synthetic, Type 2 = Natural
-  //function register(address _owner, uint _type, string _origin, uint _size, string _color, string _clarity, string _cut, string _carat) external returns (bytes32) {
   function register(address _owner, uint _type, string _origin, uint _size) external returns (bytes32) {
     require(isCA(msg.sender), "You are not allowed to call register()");
-    require(_type == 1 || _type == 2, "Type must be 1(Synthetic) or 2(Natural)");
+    require(_type == 1 || _type == 2, "Type must be 1(Synthetic) or 2(Natural)"); //Type 1 = Synthetic, Type 2 = Natural
 
     Diamond memory d;
     if(_type == 1) {
@@ -48,23 +38,18 @@ contract DiamondTracker2 {
     }
     d.origin = _origin;
     d.properties.size = _size;
-    //d.properties.color = _color;
-    //d.properties.clarity = _clarity;
-    //d.properties.cut = _cut;
-    //d.properties.carat = _carat;
     //creation of the unique ID
     d.id = sha256(abi.encodePacked(_size, _type));
 
-    if(!addDiamond(d, _owner)) {
-      revert("Diamond already exists");
-    }
+    if(!addDiamond(d, _owner))
+    revert("Diamond already exists");
 
     return d.id;
   }
 
-  function sellDiamond(bytes32 _ID, address _newOwner) {
+  function sellDiamond(bytes32 ID, address newOwner) {
     Diamond sellingDiamond;
-    sellingDiamond.id = _ID;
+    sellingDiamond.id = ID;
     require(isOwner(msg.sender, sellingDiamond), "You are not the owner of the specified diamond");
 
     Diamond[] ownedDiamonds = owners[msg.sender];
@@ -76,7 +61,7 @@ contract DiamondTracker2 {
 
     for(uint j = 0; j < diamondsList.length; j++) {
         if (diamondsList[j].id == sellingDiamond.id){
-                owners[_newOwner].push(diamondsList[j]);
+                owners[newOwner].push(diamondsList[j]);
         }
     }
 
@@ -84,24 +69,15 @@ contract DiamondTracker2 {
 
   }
 
-  function getOwnedDiamonds(address _owner)
-
-
-  //function getDiamondByIndex(uint index) external view returns (bytes32, string, DiamondType, uint, string, string, string, string) {
   function getDiamondByIndex(uint index) external view returns (bytes32, string, DiamondType, uint) {
     return (
       diamondsList[index].id,
       diamondsList[index].origin,
       diamondsList[index].d_type,
-      diamondsList[index].properties.size  //add comma
-      //diamondsList[index].properties.color,
-      //diamondsList[index].properties.clarity,
-      //diamondsList[index].properties.cut,
-      //diamondsList[index].properties.carat
+      diamondsList[index].properties.size
     );
   }
 
-  //function getDiamondByDiamonId(bytes32 id) external view returns (bytes32, string, DiamondType, uint, string, string, string, string) {
   function getDiamondByDiamonId(bytes32 id) external view returns (bytes32, string, DiamondType, uint) {
     for(uint i = 0; i < diamondsList.length; i++) {
       if(diamondsList[i].id == id){
@@ -109,11 +85,7 @@ contract DiamondTracker2 {
           diamondsList[i].id,
           diamondsList[i].origin,
           diamondsList[i].d_type,
-          diamondsList[i].properties.size  //add comma
-          //diamondsList[i].properties.color,
-          //diamondsList[i].properties.clarity,
-          //diamondsList[i].properties.cut,
-          //diamondsList[i].properties.carat
+          diamondsList[i].properties.size
         );
       }
     }
@@ -128,7 +100,6 @@ contract DiamondTracker2 {
   }
 
   function addDiamond(Diamond d, address owner) private returns (bool) {
-    //TODO I think this can be optimised
     for(uint i = 0; i < diamondsList.length; i++) {
       if(equals(d, diamondsList[i]))
         return false;
@@ -136,15 +107,13 @@ contract DiamondTracker2 {
 
     // NOTE: we expect that if diamond is already in diamondsList then it has a owner
     diamondsList.push(d);
-    // owners[d.id] = owner;
-
     owners[owner].push(d);
     return true;
   }
 
-  function isCA(address _user) private view returns (bool) {
+  function isCA(address user) private view returns (bool) {
     for(uint i = 0; i < certificate_authorities.length; i++) {
-      if(certificate_authorities[i] == _user)
+      if(certificate_authorities[i] == user)
         return true;
     }
 
@@ -166,11 +135,8 @@ contract DiamondTracker2 {
       d.id,
       d.origin,
       d.d_type,
-      d.properties.size  //add comma
-      //d.properties.color,
-      //d.properties.clarity,
-      //d.properties.cut,
-      //d.properties.carat
+      d.properties.size
     );
   }
 }
+
