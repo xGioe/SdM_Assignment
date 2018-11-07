72503811f2b6cc64ab2a7cdfdf508277fd7eb9e8
ZeCarlosCoutinho <jczelik@gmail.com>
2018-11-07 02:50:47 +0100

    Created a constant NULL_DIAMOND. Now, diamond getters verify if the diamond exists, and return NULL_DIAMOND if not

--- solidity-experiments/contracts/Diamond2.sol
+++ solidity-experiments/contracts/Diamond2.sol
@@ -9,8 +9,24 @@ contract DiamondTracker2 {
         DiamondProperties properties;
     }
 
+    //Can't declare as constant. Constant non-value types not yet supported
+    Diamond NULL_DIAMOND = Diamond({
+        id: "0x00",
+        origin: "",
+        d_type: DiamondType.Synthetic,
+        properties: DiamondProperties(0)
+    });
+
     struct DiamondProperties {
         uint size;
+    }
+
+    enum ExchangeState { Pending, Approved, Rejected, Finished} //TODO Review the states needed
+    struct DiamondExchange {
+        bytes32 diamond_id;
+        address buyer;
+        uint value; //In ether
+        ExchangeState state;
     }
 
     enum DiamondType { Synthetic, Natural }
@@ -18,6 +34,7 @@ contract DiamondTracker2 {
     //the mapping between each owner (address) and the diamons possessed
     mapping (address => Diamond[]) public owners;  
     Diamond[] diamondsList;
+    DiamondExchange[] exchanges;
 
     event diamondSold();
 
@@ -41,7 +58,7 @@ contract DiamondTracker2 {
         d.id = sha256(abi.encodePacked(_size, _type, _origin));
 
         if(!addDiamond(d, _owner))
-        revert("Diamond already exists");
+            revert("Diamond already exists");
 
         return d.id;
     }
@@ -67,16 +84,36 @@ contract DiamondTracker2 {
         emit diamondSold();
     }
 
+    /*
+    function buy(bytes32 diamond_id, uint value) external {
+        require(equals(getDiamondById(diamond_id), NULL_DIAMOND), "N"
+        DiamondExchange memory exchange;
+        exchange.diamond_id = diamond_id;
+        exchange.buyer = msg.sender;
+        exchange.value = msg.value;
+        exchange.state = ExchangeState.Pending;
+        //TODO Logic of the function
+    }*/
+
     function getDiamondByIndex(uint index) external view returns (bytes32, string, DiamondType, uint) {
-        return (
-        diamondsList[index].id,
-        diamondsList[index].origin,
-        diamondsList[index].d_type,
-        diamondsList[index].properties.size
-        );
+        if(index >= diamondsList.length) { //Assuming no diamonds are deleted from the system
+            return (
+                NULL_DIAMOND.id,
+                NULL_DIAMOND.origin,
+                NULL_DIAMOND.d_type,
+                NULL_DIAMOND.properties.size
+            );
+        } else {
+            return (
+                diamondsList[index].id,
+                diamondsList[index].origin,
+                diamondsList[index].d_type,
+                diamondsList[index].properties.size
+            );
+        }
     }
 
-    function getDiamondByDiamonId(bytes32 id) external view returns (bytes32, string, DiamondType, uint) {
+    function getDiamondByDiamondId(bytes32 id) external view returns (bytes32, string, DiamondType, uint) {
         for(uint i = 0; i < diamondsList.length; i++) {
             if(diamondsList[i].id == id){
                 return (
@@ -87,6 +124,7 @@ contract DiamondTracker2 {
                 );
             }
         }
+        return (NULL_DIAMOND.id, NULL_DIAMOND.origin, NULL_DIAMOND.d_type, NULL_DIAMOND.properties.size);
     }
 
     function getNumberOfDiamonds() external view returns (uint) {
@@ -98,7 +136,7 @@ contract DiamondTracker2 {
     }
 
     function addDiamond(Diamond d, address owner) private returns (bool) {
-        //Check if diamond already exists
+        //TODO: Check if diamond already exists
         for(uint i = 0; i < diamondsList.length; i++) {
             if(equals(d, diamondsList[i]))
               return false;
