adf21df10d0475bf946b577ce2f518196c8e5706
fedefe <federicocasano@gmail.com>
2018-11-08 15:18:04 +0100

    Add files via upload

--- solidity-experiments/contracts/Diamond2.sol
+++ solidity-experiments/contracts/Diamond2.sol
@@ -132,8 +132,10 @@ contract DiamondTracker2 {
         address _seller = msg.sender;
         for(uint i = 0; i < exchanges.length; i++) {
             if(exchanges[i].seller == _seller){
-                sellDiamond(exchanges[i].id, exchanges[i].buyer);
-                exchange.state = ExchangeState.Approved;
+                if(exchanges[i].state == ExchangeState.Pending) {
+                    sellDiamond(exchanges[i].id, exchanges[i].buyer);
+                    exchange.state = ExchangeState.Approved;
+                }
             }
         }
     }
